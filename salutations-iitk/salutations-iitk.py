9b7e0798972b90034c75d06db2a8c3bee991865b
jaynarw <jnarwaria@gmail.com>
2020-09-15 10:48:09 +0530

    Add prompt for replace/skip existing files

--- salutations-iitk/salutations-iitk.py
+++ salutations-iitk/salutations-iitk.py
@@ -6,6 +6,20 @@ import os
 import re
 import sys
 
+def pathto_dict(path):
+  for root, dirs, files in os.walk(path):
+    # tree = {root, "type":"folder", "children":[]}
+    tree = {}
+    tree[os.path.basename(root)] = [pathto_dict(os.path.join(root, d)) for d in dirs]
+    tree[os.path.basename(root)].extend(files)
+    if len(files) == 0:
+      temp = {}
+      for i in tree[os.path.basename(root)]:
+        key = list(dict.keys(i))[0]
+        temp[key] = i[key]
+        tree[os.path.basename(root)] = temp
+    return tree
+
 # Get current directory
 path = os.getcwd()
 loggedIn = False
@@ -109,6 +123,138 @@ def videoHTML(url):
    </body>
 </html>'''.format(url)
 
+# for course in answers['course_list']:
+#   summaryURL = 'https://hello.iitk.ac.in/api/' + course + '/lectures/summary'
+#   data = json.loads(r.get(summaryURL, headers={
+#     'uid':cookies['uid'],
+#     'token':cookies['token'],
+#   }).content)
+#   folder = re.sub(r'[\\\/:*?"<>|]',' - ', courseNames[course])
+
+#   weekIdx = 0
+#   topicIdx = 0
+#   videoIdx = 0
+#   fileIdx = 0
+#   totalVids = 0
+#   totalPDFs = 0
+#   currIdx = 0
+#   courseData = {}
+#   for resource in data:
+    # if (resource['videoURL'] != None):
+    #   totalVids += 1
+    # totalVids += len([v for v in resource['videosUploaded'] if v['type'] == 'original'])
+    # totalPDFs += len(resource['resources'])
+  # totalDownloads = totalPDFs + totalVids
+  # if answers['type'] == 'PDFs':
+  #   totalDownloads = totalPDFs
+  # if answers['type'] == 'Videos':
+  #   totalDownloads = totalVids
+#   for (idx, resource) in enumerate(data,1):
+#     week = resource['week']
+#     topic = resource['topic']
+#     week = re.sub(r'[\\\/:*?"<>|]',' - ', week)
+#     topic = re.sub(r'[\\\/:*?"<>|]',' - ', topic)
+#     if week not in courseData:
+#       courseData[week] = []
+#       weekIdx += 1
+#       topicIdx = 0
+#       videoIdx = 0
+#       fileIdx = 0
+#     if topic not in courseData[week]:
+#       courseData[week].append(topic)
+#       topicIdx = topicIdx + 1
+#       videoIdx = 0
+#       fileIdx = 0
+#     if(answers['type'] != 'PDFs'):
+#       if resource['videoURL'] != None:
+#         videoIdx = videoIdx + 1
+#         currIdx += 1
+        # os.makedirs(os.path.join(folder, week, str(topicIdx) + '_' + topic ), exist_ok=True)
+        # file = open(os.path.join(folder, week, str(topicIdx) + '_' + topic, str(videoIdx) + '_' + resource['title'] + '.' + 'html'), 'w+')
+        # file.write(videoHTML(resource['videoURL']))
+        # file.close()
+#       for video in [v for v in resource['videosUploaded'] if v['type'] == 'original']:
+#         videoIdx = videoIdx + 1
+#         currIdx += 1
+#         print('Downloading ' + resource['title'] + ' [' + str(currIdx) + '/' + str(totalDownloads) + ']' )
+#         download(video['path'], str(videoIdx) + '_' + resource['title'] + '.' + video['path'].split('.')[-1], folder, week, str(topicIdx) + '_' + topic)
+#     if(answers['type'] != 'Videos'):
+#       for file in resource['resources']:
+#         currIdx += 1
+#         fileIdx = fileIdx + 1
+        # print('Downloading ' + file['fileName'] + ' [' + str(currIdx) + '/' + str(totalDownloads) + ']' )
+        # download(file['fileURL'], str(fileIdx) + '_' + file['fileName'], folder, week, str(topicIdx) + '_' + topic)
+
+def courseToDirectory(data, minified=True):
+  directory = {}
+  for entry in data:
+    week = purify_name(entry["week"])
+    directory[week] = {}
+    for (topicIdx, topicEntry) in enumerate(entry['lectures'], 1):
+      topic = purify_name(topicEntry["topic"])
+      directory[week]['{}_{}'.format(topicIdx, topic)] = []
+      for (lecIdx, lec) in enumerate(topicEntry['lectures'], 1):
+        pdfs = []
+        for res in lec['resources']:
+          # if res['fileName'] in 
+          if minified:
+            pdfs.append('{}_{}'.format(lecIdx, res['fileName']))
+          else:
+            pdfs.append(['{}_{}'.format(lecIdx, res['fileName']), res['fileURL'], 'file'])
+        directory[week]['{}_{}'.format(topicIdx, topic)].extend(pdfs)
+        title = lec['title']
+        if lec['videoURL'] != None:
+          if minified:
+            directory[week]['{}_{}'.format(topicIdx, topic)].append('{}_{}.html'.format(lecIdx, title))
+          else:
+            directory[week]['{}_{}'.format(topicIdx, topic)].append(['{}_{}.html'.format(lecIdx, title), lec['videoURL'], 'url'])
+        for video in lec['videosUploaded']:
+          if video['type'] == 'original':
+            name = '{}_{}'.format(lecIdx, title) + '.' + video['path'].split('.')[-1]
+            if minified:
+              directory[week]['{}_{}'.format(topicIdx, topic)].append(name)
+            else:
+              directory[week]['{}_{}'.format(topicIdx, topic)].append([name, video['path'], 'video'])
+  return directory
+
+def pprint(x):
+  print(json.dumps(x, indent=2, sort_keys=True))
+def purify_name(x):
+  return re.sub(r'[\\\/:*?"<>|]',' - ', x)
+
+replace_question = [
+    {
+      'type': 'list',
+      'name': 'action',
+      'message': '',
+      'choices': ['Replace', 'Replace All', 'Skip', 'Skip All'],
+      'default': 2,
+    }
+]
+replace_answer = {
+  'action': None
+}
+
+def skip(folder, week, topic, file_name):
+  global replace_answer
+  if os.path.isfile(os.path.join(folder, week, topic, file_name)):
+    if replace_answer['action'] == None:
+      replace_question[0]["message"] = "File \"{} => {} => {} => {}\" already exists.. Download and Replace?".format(folder, week, topic, file_name)
+      replace_answer = prompt(replace_question)
+    if replace_answer['action'] == 'Skip':
+      replace_answer['action'] = None
+      return True
+    elif replace_answer['action'] == 'Skip All':
+      return True
+    elif replace_answer['action'] == 'Replace':
+      replace_answer['action'] = None
+      return False
+    else:
+      return False
+  else:
+    return False
+
+
 for course in answers['course_list']:
   summaryURL = 'https://hello.iitk.ac.in/api/' + course + '/lectures/summary'
   data = json.loads(r.get(summaryURL, headers={
@@ -116,57 +262,70 @@ for course in answers['course_list']:
     'token':cookies['token'],
   }).content)
   folder = re.sub(r'[\\\/:*?"<>|]',' - ', courseNames[course])
-
-  weekIdx = 0
-  topicIdx = 0
-  videoIdx = 0
-  fileIdx = 0
+  temp = []
   totalVids = 0
   totalPDFs = 0
-  currIdx = 0
-  courseData = {}
-  for resource in data:
-    if (resource['videoURL'] != None):
+  for res in data:
+    if (res['videoURL'] != None):
       totalVids += 1
-    totalVids += len([v for v in resource['videosUploaded'] if v['type'] == 'original'])
-    totalPDFs += len(resource['resources'])
+    totalVids += len([v for v in res['videosUploaded'] if v['type'] == 'original'])
+    totalPDFs += len(res['resources'])
+
+    toInsert = True
+    for entry in temp:
+      if entry['week'] == res['week']:
+        entry['lectures'].append(res)
+        toInsert = False
+    if toInsert:
+      temp.append({"week" : res['week'], 'lectures': [res]})
+  for entry in temp:
+    lecs = list(entry['lectures'])
+    entry['lectures'] = []
+    for lecture in lecs:
+      topicExists = False
+      for lec_2 in entry['lectures']:
+        if lecture['topic'] == lec_2['topic']:
+          topicExists = True
+          lec_2['lectures'].append(lecture)
+          break
+      if topicExists == False:
+        entry['lectures'].append({
+          "topic": lecture['topic'],
+          "lectures": list([lecture])
+        })
   totalDownloads = totalPDFs + totalVids
   if answers['type'] == 'PDFs':
     totalDownloads = totalPDFs
   if answers['type'] == 'Videos':
     totalDownloads = totalVids
-  for (idx, resource) in enumerate(data,1):
-    week = resource['week']
-    topic = resource['topic']
-    week = re.sub(r'[\\\/:*?"<>|]',' - ', week)
-    topic = re.sub(r'[\\\/:*?"<>|]',' - ', topic)
-    if week not in courseData:
-      courseData[week] = []
-      weekIdx += 1
-      topicIdx = 0
-      videoIdx = 0
-      fileIdx = 0
-    if topic not in courseData[week]:
-      courseData[week].append(topic)
-      topicIdx = topicIdx + 1
-      videoIdx = 0
-      fileIdx = 0
-    if(answers['type'] != 'PDFs'):
-      if resource['videoURL'] != None:
-        videoIdx = videoIdx + 1
-        currIdx += 1
-        os.makedirs(os.path.join(folder, week, str(topicIdx) + '_' + topic ), exist_ok=True)
-        file = open(os.path.join(folder, week, str(topicIdx) + '_' + topic, str(videoIdx) + '_' + resource['title'] + '.' + 'html'), 'w+')
-        file.write(videoHTML(resource['videoURL']))
-        file.close()
-      for video in [v for v in resource['videosUploaded'] if v['type'] == 'original']:
-        videoIdx = videoIdx + 1
-        currIdx += 1
-        print('Downloading ' + resource['title'] + ' [' + str(currIdx) + '/' + str(totalDownloads) + ']' )
-        download(video['path'], str(videoIdx) + '_' + resource['title'] + '.' + video['path'].split('.')[-1], folder, week, str(topicIdx) + '_' + topic)
-    if(answers['type'] != 'Videos'):
-      for file in resource['resources']:
-        currIdx += 1
-        fileIdx = fileIdx + 1
-        print('Downloading ' + file['fileName'] + ' [' + str(currIdx) + '/' + str(totalDownloads) + ']' )
-        download(file['fileURL'], str(fileIdx) + '_' + file['fileName'], folder, week, str(topicIdx) + '_' + topic)
+  formatted_data = courseToDirectory(temp, minified=False)
+  curr_idx = 1
+  for week in formatted_data:
+    for topic in formatted_data[week]:
+      for file_data in formatted_data[week][topic]:
+        if file_data[2] == 'video' and answers['type'] != 'PDFs':
+          if skip(folder, week, topic, file_data[0]):
+            curr_idx+=1
+            continue
+          print('Downloading ' + file_data[0] + ' [' + str(curr_idx) + '/' + str(totalDownloads) + ']' )
+          download(file_data[1], file_data[0], folder, week, topic)
+        elif file_data[2] == 'url' and answers['type'] != 'PDFs':
+          if skip(folder, week, topic, file_data[0]):
+            curr_idx+=1
+            continue
+          os.makedirs(os.path.join(folder, week, topic ), exist_ok=True)
+          file = open(os.path.join(folder, week, topic, file_data[0]), 'w+')
+          file.write(videoHTML(file_data[1]))
+          file.close()
+        elif file_data[2] == 'file' and answers['type'] != 'Videos':
+          if skip(folder, week, topic, file_data[0]):
+            curr_idx+=1
+            continue
+          print('Downloading ' + file_data[0] + ' [' + str(curr_idx) + '/' + str(totalDownloads) + ']' )
+          download(file_data[1], file_data[0], folder, week, topic)
+        curr_idx += 1
+
+
+  # pprint(pathto_dict(os.path.join(path, folder)))
+# print(pathto_dict(path))
+# os.path.join(path, )
